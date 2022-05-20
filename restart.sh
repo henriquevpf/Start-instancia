177cabeb05a0cfb0c436e8589279fcc9040745b5
henriquevpf <105055254+henriquevpf@users.noreply.github.com>
2022-05-20 17:39:34 -0300

    Create restart.sh

---
+++ restart.sh
@@ -0,0 +1,29 @@
+clear
+  printf "${WHITE} 💻 Digite o nome da instancia a ser reiniciada (Utilizar Letras minusculas):${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " instancia_add
+
+  printf "${WHITE} 💻 Iniciando pm2 (backend)...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - vespertinewebot <<EOF
+  cd /home/vespertinewebot/${instancia_add}/backend
+  pm2 restart dist/server.js --name ${instancia_add}-vespertinewebot-backend
+EOF
+
+  sleep 2
+
+  printf "${WHITE} 💻 Iniciando pm2 (frontend)...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - vespertinewebot <<EOF
+  cd /home/vespertinewebot/${instancia_add}/frontend
+  pm2 restart server.js --name ${instancia_add}-vespertinewebot-frontend
+  pm2 save
+EOF
+
+  sleep 2
